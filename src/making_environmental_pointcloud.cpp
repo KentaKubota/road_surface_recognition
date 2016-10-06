9cdc0ed8533f0f6313360d39b79595e94e463f7a
Kenta Kubota <zingl.0523@gmail.com>
2016-10-06 21:59:15 +0900

    publish cloud distinguished processing

--- src/making_environmental_pointcloud.cpp
+++ src/making_environmental_pointcloud.cpp
@@ -25,14 +25,8 @@ class Making_Envir_Cloud
         ros::NodeHandle nh;
         ros::Subscriber diag_scan_sub;
         ros::Publisher  diag_scan_20Hz_pub;
-        ros::Publisher  disting_cloud_pub;
 
-        tf::TransformListener listener;
-        std::string error_msg;
-        laser_geometry::LaserProjection projector;
-
-        sensor_msgs::PointCloud2 cloud2;
-        sensor_msgs::PointCloud2 disting_cloud2;
+        sensor_msgs::LaserScan lawn_scan;
 
         void diagScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in);
 };
@@ -41,7 +35,6 @@ class Making_Envir_Cloud
 Making_Envir_Cloud::Making_Envir_Cloud()
 {
     diag_scan_sub   = nh.subscribe<sensor_msgs::LaserScan>("/diag_scan", 100, &Making_Envir_Cloud::diagScanCallback, this);
-    disting_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/disting_cloud2", 100, false);
     diag_scan_20Hz_pub = nh.advertise<sensor_msgs::LaserScan>("/diag_scan_20Hz", 100, false);
 }
 
@@ -50,83 +43,40 @@ void Making_Envir_Cloud::diagScanCallback(const sensor_msgs::LaserScan::ConstPtr
 {
     struct timeval s, e;
     ros::Rate loop_rate(20); // 20Hz = 50ms
-    pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud (new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_save_cloud (new pcl::PointCloud<pcl::PointXYZI>);
 
     gettimeofday(&s, NULL);
 
-
-    /* Transform diagonally_hokuyo_link frame to map frame */
-    if(listener.waitForTransform(
-                scan_in->header.frame_id, 
-                "/map",
-                scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
-                ros::Duration(1.0),
-                ros::Duration(0.01),
-                &error_msg ) == 0){
-        cout <<"Failure in waitForTransform" << endl;
-        ROS_WARN("%s", error_msg.c_str());
-        return;
-    }
-
-
-    /* Transform LaserScan msg to PointCloud2 msg */
-    try{
-        projector.transformLaserScanToPointCloud("/map", *scan_in, cloud2, listener);
-    }catch(tf::TransformException e){
-        ROS_WARN("Could not transform scan to pointcloud! (%s)", e.what());
-        return;
-    }
-
-    /* Conversion PointCloud2 intensity field name to PointXYZI intensity field name */
-    cloud2.fields[3].name = "intensity";
-    pcl::fromROSMsg(cloud2, *pcl_cloud);
-
+    lawn_scan.header.stamp = scan_in->header.stamp;
+    lawn_scan.header.frame_id = scan_in->header.frame_id;
+    lawn_scan.angle_min = scan_in->angle_min;
+    lawn_scan.angle_max = scan_in->angle_max;
+    lawn_scan.angle_increment = scan_in->angle_increment;
+    lawn_scan.time_increment = scan_in->time_increment;
+    lawn_scan.range_min = scan_in->range_min;
+    lawn_scan.range_max = scan_in->range_max;
+    lawn_scan.ranges.resize(scan_in->ranges.size());
+    lawn_scan.intensities.resize(scan_in->ranges.size());
 
     /* Partition processing */
-    for(int i = 0; i < pcl_cloud->points.size(); i++){
+    for(int i = 0; i < scan_in->ranges.size(); i++){
         double normaliz = scan_in->intensities[i] / (-23.4136 * scan_in->ranges[i] * scan_in->ranges[i] - 143.118 * scan_in->ranges[i] + 2811.35);
 
-        if(normaliz >= 1)
-            pcl_cloud->points[i].intensity = 1.0;
-        else
-            pcl_cloud->points[i].intensity = 0.0;
+        if(normaliz >= 1){
+            lawn_scan.ranges[i] = scan_in->ranges[i];
+            lawn_scan.intensities[i] = scan_in->intensities[i];
+        }
+        else{
+            lawn_scan.ranges[i] = 0;
+            lawn_scan.intensities[i] = 0;
+        }
     }
 
-
-    /* Removing cloud processing */
-    pcl::PassThrough<pcl::PointXYZI> pass;
-    pass.setInputCloud (pcl_cloud);
-    pass.setFilterFieldName ("intensity");
-    pass.setFilterLimits (1.0, 1.1); /* remove cloud between 0 and 0.1 intensity value */
-    pass.setFilterLimitsNegative (false);
-    pass.filter (*pcl_save_cloud);
-
-    /* Saving processing */
-    string file_path = "/home/kenta/pcd/making_envir_cloud/";
-    string file_name;
-    static int i = 1;
-    char buf[10];
-
-    sprintf(buf, "%d", i);
-    file_name.append(file_path);
-    file_name.append(buf);
-    file_name.append(".pcd");
-    //cout << file_name << endl;
-
-    //pcl::io::savePCDFileASCII (file_name, *pcl_save_cloud);
-    i++;
-
-
-    toROSMsg (*pcl_save_cloud, disting_cloud2);
-    disting_cloud_pub.publish(disting_cloud2);
-    diag_scan_20Hz_pub.publish(scan_in);
+    diag_scan_20Hz_pub.publish(lawn_scan);
     cout << "Success in publishing   ";
 
     loop_rate.sleep();
     gettimeofday(&e, NULL);
     cout << "Compute time: " << (e.tv_sec - s.tv_sec) + (e.tv_usec - s.tv_usec)*1.0E-6 << "s" << endl;
-
 }
 
 
@@ -140,6 +90,5 @@ int main(int argc, char **argv)
 
     ros::spin();
     return 0;
-
 }
 
